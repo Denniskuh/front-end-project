bb69bd02b1500c53e5f0a1c3679071e8b2564d75
Dennis Ego <dennis.ego@student.kdg.be>
2021-02-11 21:46:16 +0100

    server update

--- src/scripts/animated-line-question/animated-line-question.js
+++ src/scripts/animated-line-question/animated-line-question.js
@@ -4,4 +4,5 @@ window.addEventListener("scroll", e => {
     let scrollTop = document.body.scrollTop ? document.body.scrollTop : document.documentElement.scrollTop; 
     let newPos = scrollTop + "px";
     document.documentElement.style.setProperty('--scrollPos', newPos);
+    console.log(scrollTop);
   });
