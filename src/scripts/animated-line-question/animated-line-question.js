ef05b0fff31f37da68fa28f94b880375f23365a7
Dennis Ego <dennis.ego@student.kdg.be>
2021-02-11 21:47:51 +0100

    server update

--- src/scripts/animated-line-question/animated-line-question.js
+++ src/scripts/animated-line-question/animated-line-question.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", e => {
     // Dealing with Safari difference.
     // look into scrollingElement https://caniuse.com/#feat=document-scrollingelement
     let scrollTop = document.body.scrollTop ? document.body.scrollTop : document.documentElement.scrollTop; 
-    let newPos = scrollTop + "px";
+    let newPos = (scrollTop*-.2) + "px";
     document.documentElement.style.setProperty('--scrollPos', newPos);
     console.log(scrollTop);
   });
